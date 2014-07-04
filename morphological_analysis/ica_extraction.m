521f8bf42ae2d07f0840698854a6c5e9eab357f4
Fernando Andreotti <fernando.andreotti@gmx.de>
2014-07-04 14:37:35 +0100

    Added boxplot

--- morphological_analysis/ica_extraction.m
+++ morphological_analysis/ica_extraction.m
@@ -101,9 +101,9 @@ while (loop)  %quit will be given as soon as complete signal is filtered
     % = decide for one component
     %Saving segment
     [maxF1,maxch] = max(F1);
-    if maxF1 > .8
+%     if maxF1 > .8
         icamorph(samp2filt) = dataICA(maxch,:);
-    end
+%     end
     
     %Augment offsets
     ssamp = endsamp+1;
--- morphological_analysis/main_morphoanalysis.m
+++ morphological_analysis/main_morphoanalysis.m
@@ -161,6 +161,53 @@ for i = 1:600
 
 end
 toc
+if debug
+    
+    % Plot about cases
+    
+    % This script plots boxplots with 2 groups  
+    % Boxplot multicolor
+    stats_ica(:,[1 3 4]) = 100.*stats_ica(:,[1 3 4]);
+    stats_ts(:,[1 3 4]) = 100.*stats_ts(:,[1 3 4]);
+    LINE_WIDTH = 1.2;
+    FONT_SIZE = 12;
+    MARKER_SIZE = 7;
+    fig1=figure(1)
+    values = []; origin = []; ext = {};
+    base = cellfun(@(x) ~isempty(regexp(x, '_l\d.mat$', 'match')), fls);   
+    c1 = cellfun(@(x) ~isempty(regexp(x, '_c1.mat$', 'match')), fls);
+    c2 = cellfun(@(x) ~isempty(regexp(x, '_c2.mat$', 'match')), fls);
+    c3 = cellfun(@(x) ~isempty(regexp(x, '_c3.mat$', 'match')), fls);
+    c4 = cellfun(@(x) ~isempty(regexp(x, '_c4.mat$', 'match')), fls);
+    c5 = cellfun(@(x) ~isempty(regexp(x, '_c5.mat$', 'match')), fls);
+
+    stats_ica = stats_ica(:,1);
+    stats_ts = stats_ts(:,1);
+    N = sum(base)+sum(c1)+sum(c2)+sum(c3)+sum(c4)+sum(c5);
+    bh=boxplot([stats_ica(base); stats_ica(c1); stats_ica(c2); stats_ica(c3); stats_ica(c4); stats_ica(c5);...
+        stats_ts(base); stats_ts(c1); stats_ts(c2); stats_ts(c3); stats_ts(c4); stats_ts(c5)], ...
+        {[repmat({'Base'},1,sum(base)) repmat({'Case1'},1,sum(c1)) repmat({'Case2'},1,sum(c2)) repmat({'Case3'},1,sum(c3)) repmat({'Case4'},1,sum(c4)) repmat({'Case5'},1,sum(c5)) ...
+        repmat({'Base'},1,sum(base)) repmat({'Case1'},1,sum(c1)) repmat({'Case2'},1,sum(c2)) repmat({'Case3'},1,sum(c3)) repmat({'Case4'},1,sum(c4)) repmat({'Case5'},1,sum(c5)) ] ...
+        [repmat({'ICA'},N,1);repmat({'TS'},N,1)]},'colors',repmat('rk',1,6),'factorgap',10,'labelverbosity','minor','labelorientation','inline')
+    set(bh,'linewidth',LINE_WIDTH);
+    ylabel('F1 (in %)','FontSize',FONT_SIZE)
+    xlabel('Recording','FontSize',FONT_SIZE)
+    set(gca,'FontSize',FONT_SIZE)
+    set(findall(gcf,'type','text'),'fontSize',FONT_SIZE);
+%     ylim([0 105])
+    xl=xlabel('Recording Number','FontSize',FONT_SIZE);
+    pos=get(xl,'Pos');
+    set(xl,'Pos',[pos(1) pos(2)-30 pos(3)])
+    save2pdf('boxplot',fig1,600)
+ 
+    % Plot about SNRmn
+    a = cellfun(@(x) strsplit(x,'_snr'), fls,'UniformOutput',0);
+    
+    b = cellfun(@(x) length(x)>1,a);
+end
+
+
+
 %% Morphological Analysis
 
 
